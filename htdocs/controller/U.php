4bff22a2e62ecd4e73d8a93b5ab9eb8ad22890b6
Honghao Liang <lianghonghao@baixing.com>
2012-09-05 16:49:35 +0800

    U页面的实现 -- 普通用户
    Signed-off-by: Honghao Liang <lianghonghao@baixing.com>

--- htdocs/controller/U.php
+++ htdocs/controller/U.php
@@ -3,8 +3,22 @@
 class U_Controller {
 	public function handle(Url $url) {
 		echo new View('header');
-		echo "This is a hello world Sample! <br />";
-		echo "UserId is " , $url->segments(1);
+		$user_id = "u" . $url->segments(1);
+
+		$user = graph($user_id);
+
+		if ($user->jobPort) {
+			$content = new View('u/normal', [
+				'user' => $user,
+			]);
+		} else {
+			$content = new View('u/normal', [
+				'user' => $user,
+			]);
+		}
+
+		echo $content;
+
 		echo new View('footer');
 
 	}
